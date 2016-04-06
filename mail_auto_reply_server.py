73f54b4f1ee2c2307211fd2cd7889543f5f85108
BillBillBillBill <billo@qq.com>
2016-04-07 01:01:57 +0800

    添加依赖

--- mail_auto_reply_server.py
+++ mail_auto_reply_server.py
@@ -1,15 +1,8 @@
 # coding: utf-8
 import smtpd
 import asyncore
-import re
-import requests
-import json
 from utils.logger import logger as log
 from utils.mail_content_parser import MailContentParser
-
-
-separator1 = "------------------------------------------------------------"
-separator2 = "============================================================"
 
 
 class MailboxServer(smtpd.SMTPServer, object):
@@ -30,7 +23,6 @@ class MailboxServer(smtpd.SMTPServer, object):
 
 
 class MailAutoReplyServer(object):
-    """A simple SMTP Inbox."""
 
     def __init__(self, port=None, address=None):
         self.port = port
@@ -55,36 +47,3 @@ class MailAutoReplyServer(object):
             asyncore.loop()
         except KeyboardInterrupt:
             log.info(u'Cleaning up')
-
-
-if __name__ == '__main__':
-
-    marserv = MailAutoReplyServer()
-
-    @marserv.collate
-    def handler(to, sender, subject, body):
-        try:
-            content = u"收件人：%s\n发件人：%s\n标题：%s\n 内容：%s" % (to, sender, subject, json.dumps(body))
-            log.info(content)
-            log.info(separator1*4)
-
-            urls = []
-            for content in body:
-                if content[0] == 'Text':
-                    # find all url
-                    urls += re.findall('href="(http://.*?)"', content[1])
-            log.info(u"发现%d个链接" % len(urls))
-            # open them
-            for url in urls:
-                try:
-                    ret = requests.get(url)
-                    status = ret.status_code
-                except Exception, e:
-                    status = e.message
-                log.info(u"url: %s  status: %s" % (url, status))
-            log.info(separator2*4)
-        except Exception, e:
-            log.error(e.message)
-
-    # Bind directly.
-    marserv.serve(address='0.0.0.0', port=25)
