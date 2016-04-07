c8b83bef757fd23b8524129f7387cc7ebd55f016
BillBillBillBill <billo@qq.com>
2016-04-07 11:10:49 +0800

    fix bug

--- run.py
+++ run.py
@@ -15,41 +15,37 @@ marserv = MailAutoReplyServer()
 
 @marserv.collate
 def handler(to, sender, subject, text, attachs, raw_data):
-    try:
-        content = u"收件人：%s\n发件人：%s\n标题：%s\n 内容：%s" % \
-                (to, sender, subject, json.dumps(text))
-        MailCls = MailItem if to.endswith("marserv.cn") else BadMailItem
-        mail_item = MailCls(
-            sender,
-            to,
-            subject,
-            "\n\n".join(text),
-            json.dumps(attachs),
-            raw_data
-        )
-        mail_item.save()
+    content = u"收件人：%s\n发件人：%s\n标题：%s\n 内容：%s" % \
+            (to, sender, subject, json.dumps(text))
+    MailCls = MailItem if to.endswith("marserv.cn") else BadMailItem
+    mail_item = MailCls(
+        sender,
+        to,
+        subject,
+        "\n\n".join(map(lambda t: t[1], text)),
+        json.dumps(attachs),
+        raw_data
+    )
+    mail_item.save()
 
-        log.info(content)
-        log.info(separator1*4)
+    log.info(content)
+    log.info(separator1*4)
 
-        urls = []
-        for content in text:
-            # find all url
-            urls += re.findall('href="(http://.*?)"', content[1])
-        log.info(u"发现%d个链接" % len(urls))
+    urls = []
+    for content in text:
+        # find all url
+        urls += re.findall('href="(http://.*?)"', content[1])
+    log.info(u"发现%d个链接" % len(urls))
 
-        # open the urls
-        for url in urls:
-            try:
-                ret = requests.get(url)
-                status = ret.status_code
-            except Exception, e:
-                status = e.message
-            log.info(u"url: %s  status: %s" % (url, status))
-        log.info(separator2*4)
-
-    except Exception, e:
-        log.error(e.message)
+    # open the urls
+    for url in urls:
+        try:
+            ret = requests.get(url)
+            status = ret.status_code
+        except Exception, e:
+            status = e.message
+        log.info(u"url: %s  status: %s" % (url, status))
+    log.info(separator2*4)
 
 # Bind directly.
 marserv.serve(address='0.0.0.0', port=25)
